282cf2345e3cd9bd2c9c3ca4c9f23f69d4236cd3
phisonk <phisonk@hotmail.com>
2019-05-07 08:24:30 +0000

    update

--- smart_soi2.py
+++ smart_soi2.py
@@ -1,9 +1,12 @@
+
 from flask import Flask, request , jsonify
+from flask_cors import CORS
 from flask_restful import Resource, Api
 import json
 import random
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 import requests
@@ -37,63 +40,55 @@ def traffic_percentage(x):
 		return 75
 	else:
 		return 100
+def traffic(data):
+	aa = 10
+	#x = data["Distance"]
+	x = data.split("-")
+	lineNotify(x[0],"admin")
+	if int(x[0]) > aa:
+		lineNotify("No","admin")
+		return "No_traffic"
+	elif int(x[0]) < aa and int(x[1]) >aa and int(x[2])>aa:
+		lineNotify("Light","admin")
+		return "Light_traffic"
+	elif int(x[0]) < aa and int(x[1]) < aa and int(x[2])>aa:
+		lineNotify("Medium","admin")
+		return "Medium_traffic"
+	elif int(x[0])<aa and int(x[1])<aa and int(x[2])<aa:
+		lineNotify("High","admin")
+		return "High_traffic"
+def sugess(status1,status2):
+	if status1 == "High_traffic" and status2 == "High_traffic":
+		return "Stay in the lane"
+	elif status1 == "High_traffic" and status2 != "High_traffic":
+		return "Change to the right lane"
+	else:
+		return "Stay in the lane"
+@app.route('/smart_soi/cie/<location>',methods=['GET','POST'])
+def get_soi(location):
+        url = "http://35.208.101.91:8080/sensors/project_id/smart_soi/cie/" + location
+        response = requests.get(url)
+	url1 = "https://www.aismagellan.io/api/things/pull/f91baf10-659e-11e9-96dd-9fb5d8a71344"
+	response1 = requests.get(url1)
+	rawdata = response1.json()
+	print(rawdata)
+	rawdatas = rawdata["Distance"]
+	rawdatas = rawdatas.split("n")
+	statusx = traffic(rawdatas[0])
+	statusy = traffic(rawdatas[1])
+	sugessx = sugess(statusy,statusx)
+        smart_soi_status = response.json()
+        data = []
+        for i in range(len(smart_soi_status)):
+                coming_data = {'name': location, 'percentage':traffic_percentage(smart_soi_status[i]["sensor_data"]),'location': 'soi1','status_soi1': statusx,'status_soi2': statusy,'sugession':sugessx}
+                print(data)
+		#coming_data = {'Alley':'soi1','alley_status':'Heavy_traffic','Location':[20,30]}
+                #data.append(coming_data)
+		url1 = "http://35.231.245.160:5000/notify/admin/"+statusy+sugessx+"/none"
+		response2 = requests.get(url1)
+                print(response2)
+                print(smart_soi_status[i]["sensor_data"])
+        return json.dumps(coming_data)
 
-def lineNotify(message,type):
-    payload = {'message':message}
-    return _lineNotify(payload,type)
-
-def notifyFile(filename):
-    file = {'imageFile':open(filename,'rb')}
-    payload = {'message': 'test'}
-    return _lineNotify(payload,file)
-
-def notifyPicture(url,type):
-    payload = {'message':" ",'imageThumbnail':url,'imageFullsize':url}
-    return _lineNotify(payload,type)
-
-def notifySticker(stickerID,stickerPackageID,type):
-    payload = {'message': " ",'stickerPackageId':stickerPackageID,'stickerId':stickerID}
-    return _lineNotify(payload,type)
-
-def _lineNotify(payload,type,file=None):
-    import requests
-    url = 'https://notify-api.line.me/api/notify'
-    if type == "admin":
-    	token = 'rUVBdKwkDNTvfN5QL5mMCZktU5llOyjk5KLDil5oJ5D'	#EDIT
-    elif type == "user":
-    	token = 'u5dhlrfoKQYbwDTVXQlo4jA5Wq1x4UShD9GrWpgRQVV'
-    else:
-    	return "Not Available"
-    headers = {'Authorization':'Bearer '+token}
-    return requests.post(url, headers=headers , data = payload, files=file)
-
-class smart_soi_status(Resource):
-        def get(self, location):
-		notifySticker(34,2,"user")
-                url = "http://35.208.101.91:8080/sensors/project_id/smart_soi/cie/" + location
-                response = requests.get(url)
-                smart_soi_status = response.json()
-                data = []
-                for i in range(len(smart_soi_status)):
-                        coming_data = [{'name': location}, {'percentage':traffic_percentage(smart_soi_status[i]["sensor_data"])},{'location': 'soi1'}]
-                        print(data)
-			#coming_data = {'Alley':'soi1','alley_status':'Heavy_traffic','Location':[20,30]}
-                        #data.append(coming_data)
-			url1 = "http://35.231.245.160:5000/notify/user/"+smart_soi_status[i]["sensor_data"]
-			response2 = requests.get(url1)
-                	print(response2)
-                        print(smart_soi_status[i]["sensor_data"])
-                return jsonify(coming_data)
-
-class line_notify_api(Resource):
-	def get(self, msg,type,sticker):
-		if msg != "none":
-			lineNotify(msg,type)
-		if sticker != "none":
-			tt = sticker.split(",")
-			notifySticker(tt[0],tt[1],type)
-		return "sent to: "+type+":"+msg
-api.add_resource(smart_soi_status, '/smart_soi/cie/<location>')
-api.add_resource(line_notify_api,'/notify/<string:type>/<string:msg>/<string:sticker>')
 if __name__ == '__main__':
      app.run(host = '0.0.0.0', port='5000', threaded=True, debug=True)
